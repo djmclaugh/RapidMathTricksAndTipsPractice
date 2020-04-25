facef186f81d057f114474bc8c6521f5ffbf2570
David McLaughlin <djmclaugh@gmail.com>
2020-04-25 14:01:43 -0400

    Apply offset to tens digit since if the offset is on the ones digit, it's too easy to check without having to use the digit sum method

--- source/util/question_generators/trick_14_generator.ts
+++ source/util/question_generators/trick_14_generator.ts
@@ -6,8 +6,6 @@ import { randomNonTrivialTwoDigitFactor } from './generator_util';
 // Using the same ratio as the book.
 const MULTIPLICATION_TO_DIVISION_RATIO = 10 / 16;
 
-const OFFSETS = [-8, -7, -6, -5, -4, -3, -2, -1, 1, 2, 3, 4, 5, 6, 7, 8];
-
 // Check multiplications and divisions
 export function generateQuestionForTrick14(): Question {
   return Math.random() < MULTIPLICATION_TO_DIVISION_RATIO
@@ -21,7 +19,7 @@ function generateMultiplication(): Question {
 
   // Half the time, the product should be wrong
   if (Math.random() < 0.5) {
-    product += randomFromArray(OFFSETS);
+    product += randomFromArray([-10, 10]);
   }
 
   return newDigitSumCheck(a, b, Operator.MULTIPLICATION, product);
@@ -34,7 +32,7 @@ function generateDivision(): Question {
 
   // Half the time, the product should be wrong
   if (Math.random() < 0.5) {
-    product += randomFromArray(OFFSETS);
+    product += randomFromArray([-10, 10]);
   }
 
   return newDigitSumCheck(product, b, Operator.DIVISION, a);
